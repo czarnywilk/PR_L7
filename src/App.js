2409a317b37a4044af9b8b7cf4eab91763cce523
Adrian <czarnywilczek631@gmail.com>
2021-04-30 10:40:57 +0200

    lab8

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { Route, Switch } from "react-router-dom";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import NavBar from "./components/navbar";
+import NotFound from "./components/notFound"
+
+function App() {
+ return (
+     <div className="container-fluid">
+       <NavBar />
+       <div className="container">
+           <div className="content">
+               <Switch>
+                   <Route path="/posts"
+                   render={props => <Posts sortBy="newest" {...props} />}
+                   />
+                   <Route path="/" exact component={Home} />
+                   <Route path="*" exact component={NotFound}/>
+               </Switch>
+           </div>
+       </div>
+     </div>
+ );
+}
+
+export default App;
+
